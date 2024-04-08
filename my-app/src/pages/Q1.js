8988ee6ced3164602c454a2311d83280a8ec0506
nawal <nawalnaz@Nawal-PC>
2024-04-08 07:07:10 -0400

    layout changes

--- my-app/src/pages/Q1.js
+++ my-app/src/pages/Q1.js
@@ -7,6 +7,16 @@ function Q1() {
     <Layout>
       <div>
         <h2>i am looking for... </h2>
+        <div style={{ position: 'absolute', width: '391.34px', height: '491.62px', left: '146px', top: '169px' }}>
+          <button style={{ boxSizing: 'border-box', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '300px 31px 47px', gap: '30px', position: 'absolute', width: '391.34px', height: '491.62px', left: '146px', top: '169px', background: '#D1E7F0', border: '15px solid #597A88', borderRadius: '30px' }}>
+            <p style={{ width: '269px', height: '80px', fontFamily: 'Source Serif 4', fontStyle: 'normal', fontWeight: '40px', fontSize: '40px', lineHeight: '55px', textAlign: 'center', color: '#597A88' }}>
+              mental health resources
+            </p>
+          </button>
+        </div>
+        <div>
+
+        </div>
       </div>
     </Layout>
 
@@ -14,3 +24,62 @@ function Q1() {
 }
 
 export default Q1;
+
+// /* Group 5 */
+
+// position: absolute;
+// width: 391.34px;
+// height: 491.62px;
+// left: 146px;
+// top: 169px;
+
+// /* Button 1 */
+
+// box-sizing: border-box;
+
+// /* Auto layout */
+// display: flex;
+// flex-direction: column;
+// align-items: center;
+// padding: 300px 31px 47px;
+// gap: 30px;
+
+// position: absolute;
+// width: 391.34px;
+// height: 491.62px;
+// left: 146px;
+// top: 169px;
+
+// background: #D1E7F0;
+// border: 15px solid #597A88;
+// border-radius: 30px;
+
+
+// /* text */
+
+// width: 269px;
+// height: 80px;
+// font - family: 'Source Serif 4';
+// font - style: normal;
+// font - weight: 400;
+// font - size: 40px;
+// line - height: 55px;
+// text - align: center;
+// color: #597A88;
+
+// /* Inside auto layout */
+// flex: none;
+// order: 0;
+// flex-grow: 0;
+
+
+// /* human-brain 4 */
+
+// position: absolute;
+// width: 212px;
+// height: 211px;
+// left: 232px;
+// top: 227px;
+
+// background: url(human-brain.png);
+
