62ee8406a152069bda4924ea45db699f45e15ff1
nawal <nawalnaz@Nawal-PC>
2024-04-08 07:54:13 -0400

    more layout items

--- my-app/src/pages/Q1.js
+++ my-app/src/pages/Q1.js
@@ -5,8 +5,8 @@ import Layout from '../components/Layout'
 function Q1() {
   return (
     <Layout>
-      <div>
-        <h2>i am looking for... </h2>
+      <div style={{ minHeight: '100vh', paddingBottom: '50px' }}>
+        <h2 style={{ width: '640px', height: '119px', left: '320px', top: '52px', fontStyle: 'normal', fontWeight: '700', fontSize: '64px', lineHeight: '81px', textAlign: 'center', color: '#FFFFFF' }}>i am looking for... </h2>
         <div style={{ position: 'absolute', width: '391.34px', height: '491.62px', left: '146px', top: '169px' }}>
           <button style={{ boxSizing: 'border-box', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '300px 31px 47px', gap: '30px', position: 'absolute', width: '391.34px', height: '491.62px', left: '146px', top: '169px', background: '#D1E7F0', border: '15px solid #597A88', borderRadius: '30px' }}>
             <p style={{ width: '269px', height: '80px', fontFamily: 'Source Serif 4', fontStyle: 'normal', fontWeight: '40px', fontSize: '40px', lineHeight: '55px', textAlign: 'center', color: '#597A88' }}>
@@ -14,8 +14,16 @@ function Q1() {
             </p>
           </button>
         </div>
-        <div>
-
+        <div style={{ position: 'relative' }}>
+          {/* Rounded Divider */}
+          <div style={{ position: 'absolute', width: '16px', height: '466px', left: 'calc(50% + 24px)', top: '105px', background: '#597A88', borderRadius: '10px' }}></div>
+        </div>
+        <div style={{ position: 'absolute', width: '391.34px', height: '491.62px', left: '753px', top: '169px' }}>
+          <button style={{ boxSizing: 'border-box', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '300px 31px 47px', gap: '30px', position: 'absolute', width: '391.34px', height: '491.62px', left: '146px', top: '169px', background: '#D1E7F0', border: '15px solid #597A88', borderRadius: '30px' }}>
+            <p style={{ width: '269px', height: '80px', fontFamily: 'Source Serif 4', fontStyle: 'normal', fontWeight: '40px', fontSize: '40px', lineHeight: '55px', textAlign: 'center', color: '#597A88' }}>
+              wellness resources
+            </p>
+          </button>
         </div>
       </div>
     </Layout>
@@ -54,7 +62,6 @@ export default Q1;
 // border: 15px solid #597A88;
 // border-radius: 30px;
 
-
 // /* text */
 
 // width: 269px;
@@ -83,3 +90,23 @@ export default Q1;
 
 // background: url(human-brain.png);
 
+// /* human-brain 3 */
+
+// position: absolute;
+// width: 183px;
+// height: 183px;
+// left: 857px;
+// top: 243px;
+
+// background: url(mental-health.png);
+
+/* Rectangle 29 */
+
+// position: absolute;
+// width: 16px;
+// height: 466px;
+// left: calc(50% - 16px/2);
+// top: 182px;
+
+// background: #597A88;
+// border-radius: 10px;
--- my-app/src/pages/Q2.js
+++ my-app/src/pages/Q2.js
@@ -1,16 +1,35 @@
-// Q2.js
+// Q1.js
 import React from 'react';
 
 import Layout from '../components/Layout'
 function Q2() {
   return (
     <Layout>
-      <div>
-        <h2>i would prefer a...</h2>
+      <div style={{ minHeight: '100vh', paddingBottom: '50px' }}>
+        <h2 style={{ width: '640px', height: '119px', left: '320px', top: '52px', fontStyle: 'normal', fontWeight: '700', fontSize: '64px', lineHeight: '81px', textAlign: 'center', color: '#FFFFFF' }}>i would prefer a... </h2>
+        <div style={{ position: 'absolute', width: '391.34px', height: '491.62px', left: '146px', top: '169px' }}>
+          <button style={{ boxSizing: 'border-box', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '300px 31px 47px', gap: '30px', position: 'absolute', width: '391.34px', height: '491.62px', left: '146px', top: '169px', background: '#D1E7F0', border: '15px solid #597A88', borderRadius: '30px' }}>
+            <p style={{ width: '269px', height: '80px', fontFamily: 'Source Serif 4', fontStyle: 'normal', fontWeight: '40px', fontSize: '40px', lineHeight: '55px', textAlign: 'center', color: '#597A88' }}>
+              casual environment
+            </p>
+          </button>
+        </div>
+        <div style={{ position: 'relative' }}>
+          {/* Rounded Divider */}
+          <div style={{ position: 'absolute', width: '16px', height: '466px', left: 'calc(50% + 24px)', top: '105px', background: '#597A88', borderRadius: '10px' }}></div>
+        </div>
+        <div style={{ position: 'absolute', width: '391.34px', height: '491.62px', left: '753px', top: '169px' }}>
+          <button style={{ boxSizing: 'border-box', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '300px 31px 47px', gap: '30px', position: 'absolute', width: '391.34px', height: '491.62px', left: '146px', top: '169px', background: '#D1E7F0', border: '15px solid #597A88', borderRadius: '30px' }}>
+            <p style={{ width: '269px', height: '80px', fontFamily: 'Source Serif 4', fontStyle: 'normal', fontWeight: '40px', fontSize: '40px', lineHeight: '55px', textAlign: 'center', color: '#597A88' }}>
+              formal environment
+            </p>
+          </button>
+        </div>
       </div>
     </Layout>
 
   );
 }
 
-export default Q2;
+export default Q1;
+
