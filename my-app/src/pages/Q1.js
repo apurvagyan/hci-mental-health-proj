0af24cdf6510c86259449516803913005ff9af85
nawal <nawalnaz@Nawal-PC>
2024-04-08 09:23:16 -0400

    cleaning up CSS

--- my-app/src/pages/Q1.js
+++ my-app/src/pages/Q1.js
@@ -2,111 +2,33 @@
 import React from 'react';
 
 import Layout from '../components/Layout'
+import mentalHealthIcon from '../images/mental-health-icon.png';
+import wellnessIcon from '../images/wellness-resources.png';
+
 function Q1() {
   return (
     <Layout>
       <div style={{ minHeight: '100vh', paddingBottom: '50px' }}>
-        <h2 style={{ width: '640px', height: '119px', left: '320px', top: '52px', fontStyle: 'normal', fontWeight: '700', fontSize: '64px', lineHeight: '81px', textAlign: 'center', color: '#FFFFFF' }}>i am looking for... </h2>
+        <h2 class='question'>i am looking for... </h2>
         <div style={{ position: 'absolute', width: '391.34px', height: '491.62px', left: '146px', top: '169px' }}>
-          <button style={{ boxSizing: 'border-box', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '300px 31px 47px', gap: '30px', position: 'absolute', width: '391.34px', height: '491.62px', left: '146px', top: '169px', background: '#D1E7F0', border: '15px solid #597A88', borderRadius: '30px' }}>
-            <p style={{ width: '269px', height: '80px', fontFamily: 'Source Serif 4', fontStyle: 'normal', fontWeight: '40px', fontSize: '40px', lineHeight: '55px', textAlign: 'center', color: '#597A88' }}>
-              mental health resources
-            </p>
+          <button class='button-options'>
+            <img src={mentalHealthIcon} class='option-one-img' alt="person's head with brain"></img>
+            <p class='text-options'>mental health resources</p>
           </button>
         </div>
         <div style={{ position: 'relative' }}>
           {/* Rounded Divider */}
-          <div style={{ position: 'absolute', width: '16px', height: '466px', left: 'calc(50% + 24px)', top: '105px', background: '#597A88', borderRadius: '10px' }}></div>
+          <div class='divider'></div>
         </div>
         <div style={{ position: 'absolute', width: '391.34px', height: '491.62px', left: '753px', top: '169px' }}>
-          <button style={{ boxSizing: 'border-box', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '300px 31px 47px', gap: '30px', position: 'absolute', width: '391.34px', height: '491.62px', left: '146px', top: '169px', background: '#D1E7F0', border: '15px solid #597A88', borderRadius: '30px' }}>
-            <p style={{ width: '269px', height: '80px', fontFamily: 'Source Serif 4', fontStyle: 'normal', fontWeight: '40px', fontSize: '40px', lineHeight: '55px', textAlign: 'center', color: '#597A88' }}>
-              wellness resources
-            </p>
+          <button class='button-options'>
+            <img src={wellnessIcon} class='option-two-img' alt="person's head with lotus flower symbol"></img>
+            <p class='text-options'>wellness resources</p>
           </button>
         </div>
       </div>
     </Layout>
-
   );
 }
 
 export default Q1;
-
-// /* Group 5 */
-
-// position: absolute;
-// width: 391.34px;
-// height: 491.62px;
-// left: 146px;
-// top: 169px;
-
-// /* Button 1 */
-
-// box-sizing: border-box;
-
-// /* Auto layout */
-// display: flex;
-// flex-direction: column;
-// align-items: center;
-// padding: 300px 31px 47px;
-// gap: 30px;
-
-// position: absolute;
-// width: 391.34px;
-// height: 491.62px;
-// left: 146px;
-// top: 169px;
-
-// background: #D1E7F0;
-// border: 15px solid #597A88;
-// border-radius: 30px;
-
-// /* text */
-
-// width: 269px;
-// height: 80px;
-// font - family: 'Source Serif 4';
-// font - style: normal;
-// font - weight: 400;
-// font - size: 40px;
-// line - height: 55px;
-// text - align: center;
-// color: #597A88;
-
-// /* Inside auto layout */
-// flex: none;
-// order: 0;
-// flex-grow: 0;
-
-
-// /* human-brain 4 */
-
-// position: absolute;
-// width: 212px;
-// height: 211px;
-// left: 232px;
-// top: 227px;
-
-// background: url(human-brain.png);
-
-// /* human-brain 3 */
-
-// position: absolute;
-// width: 183px;
-// height: 183px;
-// left: 857px;
-// top: 243px;
-
-// background: url(mental-health.png);
-
-/* Rectangle 29 */
-
-// position: absolute;
-// width: 16px;
-// height: 466px;
-// left: calc(50% - 16px/2);
-// top: 182px;
-
-// background: #597A88;
-// border-radius: 10px;
--- my-app/src/pages/Q2.js
+++ my-app/src/pages/Q2.js
@@ -2,34 +2,34 @@
 import React from 'react';
 
 import Layout from '../components/Layout'
+import sunbathing from '../images/sunbathing.png';
+import doctor from '../images/doctor.png';
+
 function Q2() {
   return (
     <Layout>
       <div style={{ minHeight: '100vh', paddingBottom: '50px' }}>
-        <h2 style={{ width: '640px', height: '119px', left: '320px', top: '52px', fontStyle: 'normal', fontWeight: '700', fontSize: '64px', lineHeight: '81px', textAlign: 'center', color: '#FFFFFF' }}>i would prefer a... </h2>
+        <h2 class='question'>i would prefer a... </h2>
         <div style={{ position: 'absolute', width: '391.34px', height: '491.62px', left: '146px', top: '169px' }}>
-          <button style={{ boxSizing: 'border-box', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '300px 31px 47px', gap: '30px', position: 'absolute', width: '391.34px', height: '491.62px', left: '146px', top: '169px', background: '#D1E7F0', border: '15px solid #597A88', borderRadius: '30px' }}>
-            <p style={{ width: '269px', height: '80px', fontFamily: 'Source Serif 4', fontStyle: 'normal', fontWeight: '40px', fontSize: '40px', lineHeight: '55px', textAlign: 'center', color: '#597A88' }}>
-              casual environment
-            </p>
+          <button class='button-options'>
+            <img src={sunbathing} class='option-one-img' alt="person's head with brain"></img>
+            <p class='text-options'>casual environment</p>
           </button>
         </div>
         <div style={{ position: 'relative' }}>
           {/* Rounded Divider */}
-          <div style={{ position: 'absolute', width: '16px', height: '466px', left: 'calc(50% + 24px)', top: '105px', background: '#597A88', borderRadius: '10px' }}></div>
+          <div class='divider'></div>
         </div>
         <div style={{ position: 'absolute', width: '391.34px', height: '491.62px', left: '753px', top: '169px' }}>
-          <button style={{ boxSizing: 'border-box', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '300px 31px 47px', gap: '30px', position: 'absolute', width: '391.34px', height: '491.62px', left: '146px', top: '169px', background: '#D1E7F0', border: '15px solid #597A88', borderRadius: '30px' }}>
-            <p style={{ width: '269px', height: '80px', fontFamily: 'Source Serif 4', fontStyle: 'normal', fontWeight: '40px', fontSize: '40px', lineHeight: '55px', textAlign: 'center', color: '#597A88' }}>
-              formal environment
-            </p>
+          <button class='button-options'>
+            <img src={doctor} class='option-two-img' alt="person's head with brain"></img>
+            <p class='text-options'>formal environment</p>
           </button>
         </div>
       </div>
     </Layout>
-
   );
 }
 
-export default Q1;
+export default Q2;
 
