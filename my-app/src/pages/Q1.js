3654f522aeeef1bd15aaf5518af912380ab3ebb1
nawal <nawalnaz@Nawal-PC>
2024-04-09 23:17:46 -0400

    minor changes

--- my-app/src/pages/Q1.js
+++ my-app/src/pages/Q1.js
@@ -9,21 +9,23 @@ function Q1() {
     <Layout>
       <div style={{ minHeight: '100vh', paddingBottom: '50px' }}>
         <h2 class='question'>i am looking for... </h2>
-        <div class='option-position' style={{ left: '146px' }}>
-          <button class='button-options'>
-            <img src={mentalHealthIcon} class='option-one-img' alt="person's head with brain"></img>
-            <p class='text-options'>mental health resources</p>
-          </button>
-        </div>
-        <div style={{ position: 'relative' }}>
-          {/* Rounded Divider */}
-          <div class='divider'></div>
-        </div>
-        <div class='option-position' style={{ left: '753px' }}>
-          <button class='button-options'>
-            <img src={wellnessIcon} class='option-two-img' alt="person's head with lotus flower symbol"></img>
-            <p class='text-options'>wellness resources</p>
-          </button>
+        <div style={{display: 'flex', justifyContent: 'center', width: '1448px', height: '614px' }}>
+          <div class='option-position' style={{ left: '146px' }}>
+            <button class='button-options'>
+              <img src={mentalHealthIcon} class='option-one-img' alt="person's head with brain"></img>
+              <p class='text-options'>mental health resources</p>
+            </button>
+          </div>
+          <div style={{ position: 'relative' }}>
+            {/* Rounded Divider */}
+            <div class='divider'></div>
+          </div>
+          <div class='option-position' style={{ left: '753px' }}>
+            <button class='button-options'>
+              <img src={wellnessIcon} class='option-two-img' alt="person's head with lotus flower symbol"></img>
+              <p class='text-options'>wellness resources</p>
+            </button>
+          </div>
         </div>
       </div>
     </Layout>
