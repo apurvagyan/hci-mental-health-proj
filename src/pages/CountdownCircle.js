47bbbf90e214838a69690b380715ba9a58d55390
Apurva Mishra <apurvagyan@gmail.com>
2024-04-22 18:44:34 -0400

    update sensor logic to hold action for option select

--- src/pages/CountdownCircle.js
+++ src/pages/CountdownCircle.js
@@ -1,82 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import './CountdownCircle.css'; 
-// import $ from 'jquery';
+import './CountdownCircle.css';
 
-// // set up host for becton center tv
-// var host = "cpsc484-02.stdusr.yale.internal:8888";
-
-// // call start method to run frames
-// $(document).ready(function () {
-//     frames.start();
-// });
-
-// // define frames for program execution
-// var frames = {
-//     socket: null,
-
-//     start: function () {
-//         var url = "ws://" + host + "/frames";
-//         frames.socket = new WebSocket(url);
-//         frames.socket.onmessage = function (event) {
-//             frames.show(JSON.parse(event.data));
-//         }
-//     },
-
-//     show: function (frame) {
-//       if (frame && frame["people"][0]) 
-//       {
-//         check_hands(frame) ;
-//     }
-//     }
-// };
-
-// function check_hands(frame) {
-//     if (frame && frame.people[0]) {
-//         var head = frame["people"][0]["joints"][26]["position"]["y"];
-//         var left = frame.people[0].joints[8].position.y;
-//         var right = frame.people[0].joints[15].position.y;
-
-//         if (left < head && right < head) {
-//             return true;
-//         }
-//     }
-//     return false;
-// }
-
-// // function check_hands(frame) 
-// // {
-// //   if (frame && frame.people[0]) 
-// //   {
-// //     //   var head = frame.people[0].joints[26].position.y;
-// //       var head = frame["people"][0]["joints"][26]["position"]["y"];
-
-// //       var left = frame.people[0].joints[8].position.y;
-
-// //       var right = frame.people[0].joints[15].position.y;
-      
-// //       if (left < head && right < head)
-// //       {
-// //         CountdownCircle();
-// //       }
-// //   }
-// //   return false; 
-// // }
-
-function CountdownCircle() {
+function CountdownCircle({ countdownStarted }) {
   const [countdown, setCountdown] = useState(5);
 
   useEffect(() => {
-    if (countdown === 0) {
-      // Navigate to a different page once countdown finishes
-      window.location.href = '/Instructions';
-    } else {
+    if (countdownStarted && countdown > 0) {
       const interval = setInterval(() => {
         setCountdown(prevCountdown => prevCountdown - 1);
       }, 1000);
 
       return () => clearInterval(interval);
+    } else if (countdown === 0) {
+      // Navigate to a different page once countdown finishes
+      window.location.href = '/Instructions';
     }
-  }, [countdown]);
+  }, [countdown, countdownStarted]);
 
   const circleStyle = {
     animationDuration: `${countdown}s` 
@@ -90,7 +29,5 @@ function CountdownCircle() {
     </div>
   );
 }
-  
-export default CountdownCircle; 
 
-
+export default CountdownCircle;
