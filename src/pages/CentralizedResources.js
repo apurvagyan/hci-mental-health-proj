5455480985483efc2ac2049afdab485b60546e3d
nawal <nawalnaz@Nawal-PC>
2024-04-23 14:15:51 -0400

    start over button

--- src/pages/CentralizedResources.js
+++ src/pages/CentralizedResources.js
@@ -5,15 +5,13 @@ import { SmallButton } from '../components/Components';
 import mentalHealthImage from '../images/mental_health_resources.png';
 
 function CentralizedResources() {
-    const [isLeftHandRaised, setIsLeftHandRaised] = useState(false);
-    const [isRightHandRaised, setIsRightHandRaised] = useState(false);
+    const [handsRaised, setHandsRaised] = useState(false);
     const [countdownStarted, setCountdownStarted] = useState(false);
 
+
     useEffect(() => {
-        // set up host for becton center tv
         const host = "cpsc484-02.stdusr.yale.internal:8888";
 
-        // call start method to run frames
         const startFrames = () => {
             const url = "ws://" + host + "/frames";
             const socket = new WebSocket(url);
@@ -30,37 +28,28 @@ function CentralizedResources() {
         return () => {
             // Clean up WebSocket connection if needed
         };
-    }, []); // Empty dependency array to ensure this effect runs only once
+    }, []);
 
     const checkHands = (frame) => {
         if (frame && frame.people[0]) {
-            const head = frame.people[0].joints[26].position.y;;
+            const head = frame.people[0].joints[26].position.y;
             const left = frame.people[0].joints[8].position.y;
             const right = frame.people[0].joints[15].position.y;
 
-            if (left < head) {
-                setIsLeftHandRaised(true);
+            if (left < head && right < head) {
+                setHandsRaised(true);
                 if (!countdownStarted) {
                     setCountdownStarted(true);
                 }
-
-            }
-            else if (right < head) {
-                setIsRightHandRaised(true);
-                if (!countdownStarted) {
-                    setCountdownStarted(true);
-                }
-            }
-            else {
-                // Reset both hand states if neither hand is raised
-                setIsLeftHandRaised(false);
-                setIsRightHandRaised(false);
+            } else {
+                setHandsRaised(false);
                 if (countdownStarted) {
                     setCountdownStarted(false);
                 }
             }
         }
     };
+
     return (
         <Layout>
             <div>
@@ -69,11 +58,9 @@ function CentralizedResources() {
                 <div class="container">
                     <img class="img-qr" src={mentalHealthImage} style={{ alignItems: 'center', width: '500px', height: '500px' }} alt="doc qr code"></img>
                 </div>
-                {isLeftHandRaised && <HandRaisedChecker countdownStarted={countdownStarted} destinationURL="/CentralizedResources" />}
-                {isRightHandRaised && <HandRaisedChecker countdownStarted={countdownStarted} destinationURL="/Final" />}
+                {handsRaised && <HandRaisedChecker countdownStarted={countdownStarted} destinationURL="/Home" />}
                 <div class="button-container">
-                    <SmallButton text="go back" />
-                    <SmallButton text="exit" />
+                    <SmallButton text="start over" />
                 </div>
 
                 {/* <p style={{ fontSize: '24px', color: 'white' }}><i><b>move your hand</b> over the button for 5 seconds to select it</i></p> */}
