097bb7198c97783358139f6d3eea6d538da0a34c
Kyra Kaya <kyra.kaya@yale.edu>
2024-04-22 23:55:19 -0400

    adds functionality for instructions:

--- src/pages/instructions.js
+++ src/pages/instructions.js
@@ -1,15 +1,18 @@
 // instructions.js
 import React, {useState, useEffect} from 'react';
+import HandRaisedChecker from './HandRaised';
 
 import Layout from '../components/Layout'
 
 function Instructions() {
- const [bothHandsRaised, setBothHandsRaised] = useState(false);
- const [handRaisedTimer, setHandRaisedTimer] = useState(null);
+  const [HandsRaised, setHandsRaised] = useState(false);
+  const [countdownStarted, setCountdownStarted] = useState(false);
 
   useEffect(() => {
+    // set up host for becton center tv
     const host = "cpsc484-02.stdusr.yale.internal:8888";
 
+    // call start method to run frames
     const startFrames = () => {
       const url = "ws://" + host + "/frames";
       const socket = new WebSocket(url);
@@ -21,41 +24,37 @@ function Instructions() {
       }
     };
 
-
     startFrames();
 
     return () => {
       // Clean up WebSocket connection if needed
     };
-  }, []);
+  }, []); // Empty dependency array to ensure this effect runs only once
 
   const checkHands = (frame) => {
     if (frame && frame.people[0]) {
-      const head = frame.people[0].joints[26].position.y;
+      const head = frame.people[0].joints[26].position.y;;
       const left = frame.people[0].joints[8].position.y;
       const right = frame.people[0].joints[15].position.y;
       
-      if (left < head && right < head) {
-        if (!handRaisedTimer) {
-          // Start timer if not started already
-          const timer = setTimeout(navigateToDifferentPage, 5000); // 5 seconds
-          setHandRaisedTimer(timer);
-        } else {
-          // Reset timer
-          clearTimeout(handRaisedTimer);
-          const timer = setTimeout(navigateToDifferentPage, 5000); // 5 seconds
-          setHandRaisedTimer(timer);
+      if (left < head && right < head)
+      {
+        setHandsRaised(true);
+        if (!countdownStarted) 
+        {
+          setCountdownStarted(true);
         }
-      } else {
-        // Hands not raised, reset timer
-        clearTimeout(handRaisedTimer);
-        setHandRaisedTimer(null);
       }
+      else 
+      {
+        // Reset both hand states if neither hand is raised
+        setHandsRaised(false);
+        if (countdownStarted) 
+        {
+          setCountdownStarted(false);
+        }
     }
-  };
-
-  const navigateToDifferentPage = () => {
-    window.location.href = "/Q1";
+    }
   };
 
   return (
@@ -64,9 +63,10 @@ function Instructions() {
         <br/>
         <p style={{ fontSize: '40px', textAlign: 'center' }}>
           answer a series of questions to receive a <b style={{ color: 'white '}}><i>personalized</i></b> mental health and wellness resource recommendation! 
-          <br/><br/>stay to the <b style={{ color: 'white '}}><i>left</i></b> or <b style={{ color: 'white '}}><i>right</i></b> to choose your answer...
-          <br/><br/> raise both hands to begin!
+          <br/><br/>raise your<b style={{ color: 'white '}}><i> left hand</i></b> or <b style={{ color: 'white '}}><i>right hand </i></b>to choose your answer...
+          <br/><br/> raise both hands for five seconds to begin!
         </p>
+        {HandsRaised && <HandRaisedChecker countdownStarted={countdownStarted} destinationURL="/Q1" />}
       {/* </div> */}
       {/* <div> help button
         <p class='help-text'>raise both hands for help!</p>
